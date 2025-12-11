cc387d1c6055ca2a33eae351910a112baecb1104
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2025-12-11 14:25:27 +0200

    fix: Randomization

--- lua/coinflip/init.lua
+++ lua/coinflip/init.lua
@@ -3,7 +3,7 @@ local M = {}
 M.flip = function()
 		vim.notify("Flipping coin...")
 		os.execute("sleep 1.5")
-		flip = math.random(0, 1)
+		flip = math.random(1, 3)
 		if flip == 1 then
 				vim.notify("Heads")
 		else
