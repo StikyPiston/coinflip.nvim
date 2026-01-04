8100b292335240ea613fd9bb274711a30a09b190
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-01-04 20:25:59 +0200

    feat: User command

--- lua/coinflip/init.lua
+++ lua/coinflip/init.lua
@@ -1,12 +1,20 @@
 local M = {}
 
 M.flip = function()
-		flip = math.random(1, 4)
+		local flip = math.random(1, 4)
 		if flip == 1 or flip == 2 then
 				vim.notify("Heads")
 		else
 				vim.notify("Tails")
 		end
+end
+
+M.setup = function()
+	vim.api.nvim_create_user_command(
+		'Coinflip',
+		M.flip(),
+		{ desc = "Flip a coin" }
+	)
 end
 
 return M
